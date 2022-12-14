0f5e2f583fce616b604cf59c4bd33a3c87c4b2d7
Ernest Archibong Udofia <ernestudosi@yahoo.com>
2022-12-14 09:59:19 -0800

    My commit for putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,15 +7,18 @@
  */
 
 int main(void)
+
 {
 	char putchar [8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 	unsigned int c;
 
 	for (for c = 0; c < sizeof(putchar); c++)
+
 	{
 		_putchar(putchar[c]);
 	}
 	
 	_putchar('\n');
+
 	return (0);
 }
