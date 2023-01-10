1778db3c43d6c64153d2023725f93cb854aae961
Ernest Archibong Udofia <ernestudosi@yahoo.com>
2023-01-10 14:56:08 -0800

    My commit for 0-isupper

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * *_isupper - a function that checks for uppercase character
- * *@c: single letter input
- * *Return: 1 if int c is lowercase, 0 if otherwise
- * */
+ *_isupper - a function that checks for uppercase character
+ *@c: single letter input
+ *Return: 1 if int c is lowercase, 0 if otherwise
+ */
 
 int _isupper(int c)
 {
