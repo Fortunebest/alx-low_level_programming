2a05ce22f4ed1b4cecd539eb50bbc4b25a63e695
Fortunebest <111252989+Fortunebest@users.noreply.github.com>
2023-01-22 16:22:38 +0100

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,4 +1,4 @@
-lude "function_pointers.h"
+#include "function_pointers.h"
 
 /**
  * array_iterator - executes a function given as
@@ -8,7 +8,7 @@ lude "function_pointers.h"
  * @action: pointer to the function to use
  *
  * Return: void
- */
+*/
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
